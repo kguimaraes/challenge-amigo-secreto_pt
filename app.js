7338fae0f33909ea03bb8970dd9e0943f65deee4
kguimaraes <66435880+kguimaraes@users.noreply.github.com>
2025-03-18 17:52:26 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    const nomeInput = document.getElementById("amigo(a)");
+    const nome = nomeInput.value.trim();
+    
+    if (nome === "") {
+        alert("Por favor, insira um nome.");
+        return;
+    }
+    
+    amigos.push(nome);
+    nomeInput.value = "";
+    atualizarLista();
+}
+
+function atualizarLista() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    
+    for (let amigo of amigos) {
+        const item = document.createElement("li");
+        item.textContent = amigo;
+        lista.appendChild(item);
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("A lista está vazia! Adicione pelo menos um nome antes de sortear.");
+        return;
+    }
+    
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceAleatorio];
+    
+    document.getElementById("resultado").innerHTML = "O amigo(a) secreto sorteado é: <strong>${amigoSorteado}</strong>";
+}
